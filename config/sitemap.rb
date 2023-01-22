108d5b3c74cb7b7da7bc55c107e849811f67b0e9
hirakida <dantuba19990326@gmail.com>
2023-01-23 02:08:17 +0900

    Fix: sitemap.rbにawsのパス追記

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,14 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.tun-ari.com"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/tun-ari"
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  'fluff-maps',
+  aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+  aws_region: 'ap-northeast-1',
+)
+
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
